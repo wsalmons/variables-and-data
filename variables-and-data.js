82287dbc51aff427acb91ef8dfcbcf57197e5dab
wsalmons <45742790+wsalmons@users.noreply.github.com>
2019-01-04 14:08:05 -0700

    created variables and data prework

---
+++ variables-and-data.js
@@ -0,0 +1,53 @@
+/*
+*Variables, Data Types, and Typing
+ */
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subtotal = 100;
+let tax = subtotal * 0.07;
+let totalwithtax = subtotal + tax;
+
+console.log("Subtotal: $"+subtotal);
+console.log("Tax(NM 0.07%): $"+tax);
+console.log("Total: $"+totalwithtax);
+
+let obj = {
+	key1: "value 1",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "value of inner object"
+	}
+};
+
+console.log(obj.obj2.obj2key1);
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "string 1 " + "string 2";
+
+console.log(string);
+
+
+
+
+
+
+
+
+
+
